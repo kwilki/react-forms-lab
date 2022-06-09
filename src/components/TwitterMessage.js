b86410d2c01aacb93a8835000bb76803f0cab7b3
kwilki <kylewilkinson14@gmail.com>
2022-06-09 16:17:43 +1000

    finished tests, error with learn-lab test

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,26 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      message: event.target.value
+    }, () => console.log(this.state))
   }
 
   render() {
+    let remainingChars = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.handleChange}/>
+        <br/>
+        <br/>
+        <span>{remainingChars}</span>
       </div>
     );
   }
